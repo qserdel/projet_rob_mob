c145d1d3b9f0aea528deb83597ea85b40595c8ff
Flosilver <florentin.b98@gmail.com>
2020-12-03 18:10:53 +0100

    ajout de la convertion en matrice opencv

--- mapping/src/mapper_client.cpp
+++ mapping/src/mapper_client.cpp
@@ -2,6 +2,15 @@
 #include "nav_msgs/GetMap.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include <cstdlib>
+#include <iostream>
+#include <memory>
+#include "gridmap2d.h"
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace gridmap_2d;
+
+
+static const std::string OPENCV_WINDOW = "Image window";
 
 int main(int argc, char **argv)
 {
@@ -15,17 +24,29 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<nav_msgs::GetMap>("static_map");
     nav_msgs::GetMap srv;
-    
+    nav_msgs::OccupancyGrid map;
+
     if (client.call(srv))
     {
         ROS_INFO("Service GetMap succeeded");
+        map = srv.response.map;
     }
     else
     {
         ROS_ERROR("Service GetMap failed");
         return 1;
     }
-    nav_msgs::OccupancyGrid map = srv.response.map;
+
     std::cout << "Map récupérée\n";
+
+    GridMap2D gridmap(map);
+
+    cv::namedWindow(OPENCV_WINDOW);
+    imshow(OPENCV_WINDOW, gridmap.binaryMap());
+    cvWaitKey(0);
+
+
+    cv::destroyWindow(OPENCV_WINDOW);
+
     return 0;
 }
