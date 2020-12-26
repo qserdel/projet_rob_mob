c7b6b5c0820f817471693eb3d0cc7a50f582083a
Flosilver <florentin.b98@gmail.com>
2020-12-26 22:24:55 +0100

    visu of the checkpoint trajectory

--- mapping/src/visualisator.cpp
+++ mapping/src/visualisator.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -9,9 +10,12 @@
 #include "nav_msgs/OccupancyGrid.h"
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Point.h"
 
 #include "gridmap2d.h"
 #include <mapping/BinaryMap.h>
+#include <planification/Checkpoints.h>
+#include <planification/ListePoints.h>
 
 using namespace gridmap_2d;
 
@@ -31,6 +35,52 @@ struct Robot
 };
 static Robot robot;
 
+struct CheckpointsTraj
+{
+    std::vector<cv::Point> points;
+    unsigned int len;
+    cv::Vec3b color;
+    unsigned int thickness;
+};
+CheckpointsTraj traj;
+
+void setCheckpointsTraj(CheckpointsTraj &cpt, const planification::Checkpoints &cp_list, cv::Vec3b col, unsigned int thickness)
+{
+    cpt.points.clear();
+    cpt.len = 0;
+    cpt.color = col;
+    cpt.thickness = thickness;
+    unsigned int mx, my;
+
+    for (const geometry_msgs::Point &pt : cp_list.response.points.points)
+    {
+        if (gridmap.inMapBounds(pt.x, pt.y))
+        {
+            gridmap.worldToMap(pt.x, pt.y, mx, my);
+            cpt.points.push_back(cv::Point(my, mx));
+            cpt.len++;
+        }
+    }
+}
+
+void showTraj(const CheckpointsTraj &cpt, cv::Mat& dest)
+{
+    for (int i=1 ; i<cpt.len ; i++)
+    {
+        cv::line(dest, cpt.points[i-1], cpt.points[i], cpt.color, cpt.thickness);
+    }
+}
+
+void printCheckPointsTraj(const CheckpointsTraj &cpt)
+{
+    std::cout << "Trajectoire de checkpoints:\n";
+    for (const cv::Point &pt : cpt.points)
+    {
+        std::cout << pt << ",";
+    }
+    std::cout << "\n";
+}
+
 void odomCallback(const nav_msgs::Odometry &msg)
 {
     geometry_msgs::Pose robot_pos = msg.pose.pose;
@@ -41,7 +91,6 @@ void odomCallback(const nav_msgs::Odometry &msg)
         robot.pos.x = my;
         robot.pos.y = mx;
     }
-    //std::cout << robot.pos << "\n";
 }
 
 int main(int argc, char **argv)
@@ -63,7 +112,12 @@ int main(int argc, char **argv)
     ros::Subscriber odom_sub = n.subscribe("odom", 1000, odomCallback);
     robot.pos = cv::Point();
     robot.radius = 10;
-    robot.color = cv::Vec3b(100, 0, 0);
+    robot.color = cv::Vec3b(200, 0, 0);
+
+    // Client du service /checkpoints
+    ros::ServiceClient checkpoint_client = n.serviceClient<planification::Checkpoints>("checkpoints");
+    planification::Checkpoints cp_srv;
+    cv::Vec3b traj_col(0,0,200);
 
     ros::Rate loop_rate(30);
     ros::Rate map_rate(1);
@@ -82,6 +136,9 @@ int main(int argc, char **argv)
     //display_map = gridmap.binaryMap();
     cv::cvtColor(gridmap.binaryMap(), display_map, cv::COLOR_GRAY2RGB);
 
+    ros::Time time = ros::Time::now();
+    ros::Duration delay_traj(10.);
+
     // continuous display
     while (ros::ok())
     {
@@ -89,10 +146,27 @@ int main(int argc, char **argv)
         {
             display_map = cv::Mat::zeros(display_map.rows, display_map.cols, CV_8U);
             cv::cvtColor(gridmap.binaryMap(), display_map, cv::COLOR_GRAY2RGB);
+
+            // Affichage du robot dans la map
             if (robot.pos.x != 0 && robot.pos.y != 0)
             {
                 cv::circle(display_map, robot.pos, robot.radius, robot.color, CV_FILLED);
             }
+
+            // Récupération de la liste de checkpoint
+            if (ros::Time::now()-time > delay_traj && checkpoint_client.call(cp_srv))
+            {
+                setCheckpointsTraj(traj, cp_srv, traj_col, 1);
+                printCheckPointsTraj(traj);
+                time = ros::Time::now();
+            }
+
+            // Affichage de la liste de checkpoints
+            if (traj.len > 0)
+            {
+                showTraj(traj, display_map);
+            }
+
             cv::imshow(OPENCV_WINDOW, display_map);
             cvWaitKey(1);
         }
