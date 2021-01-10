407fb008c2f7804bb0009ee31f08efd2b4c10ac5
Flosilver <florentin.b98@gmail.com>
2021-01-10 19:39:13 +0100

    wall follower completed

--- autonomous/src/explorer.cpp
+++ autonomous/src/explorer.cpp
@@ -4,62 +4,69 @@
 #include <iostream>
 #include <cmath>
 
+#include "ros/console.h"
 #include "geometry_msgs/Twist.h"
 
-Explorer::Explorer(): k1_(0.0f), k2_(0.0f), speed_(0.0f), d0_(0.0f), scan_sub_(), cmd_pub_()
+Explorer::Explorer() : k1_(0.0f), k2_(0.0f), speed_(0.0f), d0_(0.0f), scan_sub_(), cmd_pub_()
 {
     ros::NodeHandle n("~");
     speed_ = n.param("speed", 1.0f);
-    k1_ = n.param("k1", 0.1f);
-    k2_ = n.param("k2", 0.1f);
+    k1_ = n.param("k1", 1.0f);
+    k2_ = n.param("k2", 1.0f);
     d0_ = n.param("distance", 1.0f);
     std::string scan_topic(n.param<std::string>("scan_topic", "/scan"));
-    scan_sub_ = n.subscribe(scan_topic, 1, &Explorer::scanCallback, this);
     std::string cmd_topic(n.param<std::string>("cmd_topic", "/cmd_vel"));
+
+    // Initialize subscriber and publisher
+    scan_sub_ = n.subscribe(scan_topic, 1, &Explorer::scanCallback, this);
     cmd_pub_ = n.advertise<geometry_msgs::Twist>(cmd_topic, 1);
 }
 
-void Explorer::scanCallback(const sensor_msgs::LaserScan& msg)
+void Explorer::scanCallback(const sensor_msgs::LaserScan &msg)
 {
     size_t i_min(0);
     size_t ranges_len(msg.ranges.size());
-    for (size_t i=1 ; i<ranges_len ; i++)
+
+    //std::cout << "\n-------\n";
+    //std::cout << speed_ << "\t" << k1_ << "\t" << k2_ << "\t" << d0_ << std::endl;
+    ROS_INFO("speed: %f\tk1: %f\tk2: %f\td0: %f", speed_, k1_, k2_, d0_);
+    //std::cout << "[" << msg.angle_min << ":" << msg.angle_increment << ":" << msg.angle_max << "]" << std::endl;
+
+    // Find the index of the closest obstacle
+    for (size_t i = 1; i < ranges_len; i++)
     {
         if (msg.ranges.at(i) < msg.ranges.at(i_min))
             i_min = i;
     }
 
     size_t index_i, index_j;
-    if (i_min * 2 < ranges_len)
-    {
-        index_i = 0;
-        index_j = index_from_angle(-M_PI/4, msg);
-    }
-    else
-    {
-        index_i = ranges_len -1;
-        index_j = index_from_angle(M_PI/4, msg);
-    }
+    index_i = 0;
+    index_j = index_from_angle(-M_PI / 4, msg);
+    //std::cout << "ind_i: " << index_i << "\tind_j: " << index_j << std::endl;
 
+    // Get points of the obstacle to dertermine its direction
     float xi(0.0f), yi(0.0f), xj(0.0f), yj(0.0f);
-
     compute_pos(msg, index_i, xi, yi);
     compute_pos(msg, index_j, xj, yj);
 
-    float dist = msg.ranges[i_min];
+    std::cout << "i: [" << xi << ":" << yi << "]\n";
+    std::cout << "j: [" << xj << ":" << yj << "]\n";
 
     geometry_msgs::Twist cmd;
     cmd.linear.x = speed_;
 
+    float dist = msg.ranges.at(i_min);
+    std::cout << "dist: " << dist << std::endl;
     float tan((yi - yj) / (xi - xj));
     float angle(std::atan2(yi - yj, xi - xj));
-    cmd.angular.z = k1_ * (dist - d0_) / (speed_ * std::cos(angle)) - k2_ * std::tan(angle);
+    std::cout << "angle: " << angle << std::endl;
+    cmd.angular.z = k1_ * (dist - d0_) / (speed_ * std::cos(angle)) - k2_ * -tan;
     cmd_pub_.publish(cmd);
 
-    std::cout << cmd << std::endl;
+    //std::cout << cmd << std::endl;
 }
 
-void Explorer::compute_pos(const sensor_msgs::LaserScan& msg, const size_t index, float& x, float& y)
+void Explorer::compute_pos(const sensor_msgs::LaserScan &msg, const size_t index, float &x, float &y)
 {
     float angle(msg.angle_min + msg.angle_increment * index);
     float dist(msg.ranges.at(index));
@@ -67,7 +74,7 @@ void Explorer::compute_pos(const sensor_msgs::LaserScan& msg, const size_t index
     y = dist * std::sin(angle);
 }
 
-size_t Explorer::index_from_angle(float angle, const sensor_msgs::LaserScan& msg)
+size_t Explorer::index_from_angle(float angle, const sensor_msgs::LaserScan &msg)
 {
     return (angle - msg.angle_min) / msg.angle_increment;
 }
